e4998a06618f8f7f282422b9bb487fc24a62250b
Kush V. <148393667+Kvapiwala@users.noreply.github.com>
2024-09-10 13:54:11 -0700

    Add files via upload

--- ConcentricSquare.py
+++ ConcentricSquare.py
@@ -1,33 +1,31 @@
 import turtle as trtl
-trtl.screensize(800, 800, "light green")
+trtl.Screen().bgcolor("light green")
 painter = trtl.Turtle()
-painter.speed(0)
+painter.speed(15)
 
-#Dictionary for the colors of each square
-colors = {
-    "colors": ["red", "blue", "yellow", "green", "purple"]
-}
-
-x = 0 
-y = 0
+#init colors, starting position, incrementation, and first square size
+colors_list = ["red", "blue", "green", "purple", "gold"]
+x = -125 
+y = -125
 move = 25
-forward = 50
+forward = 300
 
 #Draw squares
-while(x <= 0 , y <= 0):
-    for i in range(5):
-        for i in range(4):
-            x = x - move
-            y = y - move
-            forward = (forward + (move * 2))
-            painter.forward(forward)
-            painter.left(90)
-            painter.penup()
-            painter.goto(x,y)
-            painter.pendown()
-
-
+for i in range(5):
+    painter.goto(x,y)
+    x = x + move
+    y = y + move
+    forward = (forward - (move * 2)) #Incrementation
+    painter.fillcolor(colors_list[i]) #Iterate thru the colors list, draw each square based on each color from the list
+    painter.begin_fill()
+    for i in range(4):
+        painter.forward(forward)
+        painter.left(90)
+    painter.end_fill()
+    painter.penup()
+    painter.goto(x,y)
+    painter.pendown()
+painter.hideturtle()
 
 wn = trtl.Screen()
 wn.mainloop()
-
