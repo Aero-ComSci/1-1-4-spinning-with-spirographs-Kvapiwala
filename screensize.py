e471fb35c66749ad48b2ff33dc8bffea72a2f42b
Kush V. <148393667+Kvapiwala@users.noreply.github.com>
2024-10-04 16:49:09 -0700

    Add files via upload

--- screensize.py
+++ screensize.py
@@ -1,35 +1,20 @@
 
 import turtle as trtl
-trtl.Screen().bgcolor("light green")
-trtl.setup(800,800)
-painter = trtl.Turtle()
-painter.speed(15)
-
-
-#init colors, starting position, incrementation, and first square size
-colors_list = ["light blue", "maroon", "orchid", "dark green", "gold","light blue", "maroon", "orchid", "dark green", "gold","light blue", "maroon", "orchid", "dark green", "gold", "red"]
-x = -375 
-y = -375
-move = 75
-forward = 900
-painter.pensize(50)
-
-#Draw 5 squares
-for i in range(5):
-    painter.goto(x,y)
-    x = x + move
-    y = y + move
-    forward = (forward - (move * 2)) #Incrementation
-    painter.pencolor(colors_list[i]) #Iterate thru the colors list, draw each square based on each color from the list
-    painter.begin_fill()
-    for i in range(4):
-        painter.forward(forward)
-        painter.left(90)
-    painter.end_fill()
-    painter.penup()
-    painter.goto(x,y)
-    painter.pendown()
-painter.hideturtle()
-
 wn = trtl.Screen()
+wn.setup(width=800,height=800)
+painter = trtl.Turtle()
+
+radius = 50 #size of circle
+circles = 5
+
+width=800 #total width of the screen how far the circles will reach
+distance = (width - (2*radius))/(circles-1) #calculation for how far the circles are apart, equidistant
+
+for i in range (circles):
+    x = -350 + i * distance #because the circle radius is 50 we need to subtract the border lenght by 50 for the starting positon so the edge touches.
+    painter.penup()
+    painter.goto(x,0)
+    painter.pendown()
+    painter.circle(radius)
+
 wn.mainloop()
